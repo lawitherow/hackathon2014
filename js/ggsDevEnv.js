dba3c82b43ca49bdb07d851b07cace9d12e9cc69
ggs-mmatic <mmatic@goodgamestudios.com>
2014-12-17 12:56:08 +0100

    Slider Left with child slider
    Left slider implemented with child slider

--- js/ggsDevEnv.js
+++ js/ggsDevEnv.js
@@ -4,18 +4,32 @@
 
 var ggsDevEnv = (function(){
 
-	_slideOut = function() {
-		document.getElementById('main-navi').style.left = "0px";
+	_slideOut = function(elem,value) {
+		elem.style.left = value;
+	};
+
+	_slideIn = function(elem,value) {
+		elem.style.left = value;
+	};
+
+	_setMainClickEvents = function() {
+		for(var i=0; i<document.getElementById('main-navi').children[0].children.length; i++) {
+			var liElem = document.getElementById('main-navi').children[0].children[i];
+			liElem.addEventListener("onclick", function(){ggsDevEnv.slideOut(document.getElementById('sub-navi'), '300px')}, false)
+		}
 	};
 
 	_init = function() {
-		_slideOut();
+		document.getElementById('main-navi').addEventListener("mouseover", function(){ggsDevEnv.slideOut(document.getElementById('main-navi'), '0px')}, false);
+		document.getElementById('main-navi').addEventListener("mouseout", function(){ggsDevEnv.slideIn(document.getElementById('main-navi'), '-230px')}, false);
+		_setMainClickEvents();
 	};
 
 	return {
-		init : _init
+		init : _init,
+		slideOut : _slideOut,
+		slideIn : _slideIn
 	}
-
 })();
 
 window.onload = ggsDevEnv.init;
